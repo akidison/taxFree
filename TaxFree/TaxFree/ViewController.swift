0709397818b39f413f87c5fcea7be89bfd1ec038
Andrey Kiselev <akidis.developer@gmail.com>
2019-06-18 22:54:51 +0300

    - create project to github

---
+++ TaxFree/TaxFree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaxFree
+//
+//  Created by Andrey Kiselev on 18/06/2019.
+//  Copyright © 2019 Andrey Kiselev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
